fd954f326dbb6c8e7680630f408ba10b3ee7b83a
Ruoshi <me@ruoshi.li>
2026-01-03 22:12:13 -0800

    feat: add PDF progress bar and improve LTBase rendering

--- ltbase_pdf.js
+++ ltbase_pdf.js
@@ -3,17 +3,44 @@ import * as pdfjsLib from
 
 (async function () {
     const canvas = document.getElementById('pdfCanvas');
+    const progressBar = document.getElementById('ltbase-pdf-progress-bar');
 
     let currentPage = 1;
+    let timeOutId = -1;
     const ctx = canvas.getContext('2d');
 
     pdfjsLib.GlobalWorkerOptions.workerSrc =
         "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/5.4.149/pdf.worker.min.mjs";
 
+    const restartProgress = () => {
+        if (!progressBar) {
+            return;
+        }
+        if (timeOutId > 0) {
+            window.clearTimeout(timeOutId);
+        }
+        const animations = progressBar.getAnimations();
+
+        animations.forEach(animation => {
+            animation.cancel();
+            animation.play();
+        });
+        // progressBar.classList.remove('ltbase-pdf-progress__bar-animating');
+        // void progressBar.offsetWidth; // force reflow so the animation restarts
+        // progressBar.classList.add('ltbase-pdf-progress__bar-animating');
+
+        timeOutId = window.setTimeout(async () => {
+            if (!pdfDoc) {
+                return;
+            }
+            await renderNextPage();
+        }, 15000);
+    };
+
     const pdfDoc = await pdfjsLib.getDocument("https://raw.githubusercontent.com/Lychee-Technology/lychee.technology/refs/heads/main/img/LTBase.pdf").promise;
     async function renderPage(num) {
         const page = await pdfDoc.getPage(num);
-        const viewport = page.getViewport({ scale: 1.5 });
+        const viewport = page.getViewport({ scale: 2.5 });
 
         canvas.height = viewport.height;
         canvas.width = viewport.width;
@@ -27,22 +54,29 @@ import * as pdfjsLib from
         currentPage = num;
     }
 
-    canvas.addEventListener('click', (e) => {
+    const renderNextPage = async () => {
         if (!pdfDoc) {
             return;
         }
 
-        if (currentPage >= pdfDoc.numPages) {
-            currentPage = currentPage % pdfDoc.numPages;
-        }
         currentPage++;
+        if (currentPage > pdfDoc.numPages) {
+            currentPage = 1;
+        }
 
-        renderPage(currentPage);
+        await renderPage(currentPage);
+        restartProgress();
+    }
+
+    canvas.addEventListener('click', async (e) => {
+        await renderNextPage();
     });
 
     for (let i = pdfDoc.numPages; i >= 1; i--) {
         await renderPage(i);
     }
-    
+
     canvas.style.visibility = 'visible';
+    progressBar.style.visibility = 'visible';
+    restartProgress();
 })();
