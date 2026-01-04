50cb0aee33615d87a4feb5f8fb21ffeaa7c6650d
Ruoshi <me@ruoshi.li>
2026-01-03 21:33:06 -0800

    use pdf for ltbase

---
+++ ltbase_pdf.js
@@ -0,0 +1,48 @@
+import * as pdfjsLib from
+    "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/5.4.149/pdf.min.mjs";
+
+(async function () {
+    const canvas = document.getElementById('pdfCanvas');
+
+    let currentPage = 1;
+    const ctx = canvas.getContext('2d');
+
+    pdfjsLib.GlobalWorkerOptions.workerSrc =
+        "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/5.4.149/pdf.worker.min.mjs";
+
+    const pdfDoc = await pdfjsLib.getDocument("/img/LTBase.pdf").promise;
+    async function renderPage(num) {
+        const page = await pdfDoc.getPage(num);
+        const viewport = page.getViewport({ scale: 1.5 });
+
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        const renderContext = {
+            canvasContext: ctx,
+            viewport: viewport
+        };
+
+        await page.render(renderContext).promise;
+        currentPage = num;
+    }
+
+    canvas.addEventListener('click', (e) => {
+        if (!pdfDoc) {
+            return;
+        }
+
+        if (currentPage >= pdfDoc.numPages) {
+            currentPage = currentPage % pdfDoc.numPages;
+        }
+        currentPage++;
+
+        renderPage(currentPage);
+    });
+
+    for (let i = pdfDoc.numPages; i >= 1; i--) {
+        await renderPage(i);
+    }
+    
+    canvas.style.visibility = 'visible';
+})();
